12319e6e144c0cd7031d7f1b1efe0c8e07a9931f
Dominik Polzer <dominik.polzer1990@gmail.com>
2021-02-14 17:26:31 +0100

    added little SwiftUI project

---
+++ HackerNews - SwiftUI/Models/PostData.swift
@@ -0,0 +1,24 @@
+//
+//  PostData.swift
+//  HackerNews - SwiftUI
+//
+//  Created by Dominik Polzer on 13.02.21.
+//
+
+import Foundation
+
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ HackerNews - SwiftUI/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  HackerNews - SwiftUI
+//
+//  Created by Dominik Polzer on 13.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                        
+                    })
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
